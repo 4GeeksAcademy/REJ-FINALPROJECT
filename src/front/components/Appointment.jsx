cce70e9d1ee990ac861b4666a1c65cdb325a9c36
ELKIN RONDON <elkin_rondon@hotmail.com>
2025-07-31 02:05:27 +0000

    [Modal para editar citas] 	modified:   src/app.py 	modified:   src/front/components/Appointment.jsx 	new file:   src/front/components/Edit_Appointment.jsx 	modified:   src/front/pages/Home.css 	modified:   src/front/pages/Home_Stylist.jsx 	modified:   src/front/routes.jsx

--- src/front/components/Appointment.jsx
+++ src/front/components/Appointment.jsx
@@ -1,16 +1,23 @@
 import React, {useState} from "react";
+import Edit_Appointment from "../components/Edit_Appointment"
 
-const Appointment = ({Appointment, viewAppointment, index}) => {
-    
+
+
+const Appointment = ({appointment, viewAppointment, editAppointment, setModalIsOpen, modalIsOpen, index}) => {
+  
+
     return (
         
         <li className="list-group-item list-group-item-danger" key={index} style ={{ height:"20%"}}>
             <div className="row  my-1 mx-2 ">
                 <div className="col-9">
-                    <h4>{Appointment.date +"  " + Appointment.user}</h4>
+                    <h4>{appointment.date +"  " + appointment.user}</h4>
                 </div>
                 <div className="col-3">
-                  <button type="button" className="btn btn-light"  onClick={() => { viewAppointment(index,Appointment.user_objeto) }}>Details</button>
+                  <button type="button" className="btn boton"  onClick={() => { viewAppointment(index,appointment.user_objeto) }}>Details</button>
+                  <button type="button" className="btn boton mx-2" onClick={() => setModalIsOpen(true)}>Edit</button>
+                  <Edit_Appointment appointment={appointment} editAppointment={editAppointment} isOpen={modalIsOpen} />
+
                 </div>
             </div>
         </li> 
